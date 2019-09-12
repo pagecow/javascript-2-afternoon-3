c36416681d2bdd903094565bf310eadff99c0eea
pagecow <pagecow@gmail.com>
2019-09-11 18:04:09 -0600

    finished 1-4; need to finish 5-7

--- practice.js
+++ practice.js
@@ -27,7 +27,10 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+function first(array, callback) {
+  return callback(array[0]);
+}
+
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -47,7 +50,9 @@ first(names, function(firstName){
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+function last(array, callback){
+  return callback(array[array.length - 1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -65,7 +70,9 @@ last(names, function(lastName){
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+function multiply(num1, num2, callback){
+  return callback(num1*num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -84,7 +91,14 @@ multiply(4, 3, function(answer){
   If the name does not exist, invoke the callback with false as the argument.
 */
 
-//Code Here 
+function contains(array, name, callback){
+  for (i=0; i >= array.length; i++){
+    if (array[i] === name) {
+      return true
+    }
+  }
+  return false
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -105,7 +119,9 @@ contains(names, 'Colt', function(result){
   Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 */
 
-//Code Here
+function uniq(array, callback){
+  
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
